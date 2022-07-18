12bfa007f3d30b9ec752aa0bbcfbdd1002298851
mustafa3306 <mustafaworkkok@gmail.com>
2022-07-18 18:01:58 +0300

    cmt

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ item }) => {
             <img src={'https://image.tmdb.org/t/p/original' + backdrop_path} className="card-img-top" alt="..." />
             <div className="card-body" >
                 <h5 className="card-title" > {title}</h5 >
-                <p className="card-text" > Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text" >{overview}</p>
                 < a href="#" className="btn btn-primary" > {vote_average}</a >
             </div >
         </div >

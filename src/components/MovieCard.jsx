0431d9db3efd734743d964ce458e98149d0e17c6
mustafa3306 <mustafaworkkok@gmail.com>
2022-07-19 14:23:39 +0300

    cmt

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,11 @@ const MovieCard = ({ item }) => {
     const { title, backdrop_path, vote_average, overview } = item
     console.log(item)
     return (
-        <div className="card" style={{ width: '18rem' }} >
+        <div className="card" style={{ width: '18rem' }}>
             <img src={'https://image.tmdb.org/t/p/original' + backdrop_path} className="card-img-top" alt={title} />
             <div className="card-body" >
                 <h5 className="card-title" > {title}</h5 >
-                <p className="card-text" >{overview}</p>
+                {/* <p className="card-text" >{overview}</p> */}
                 < a href="#" className="btn btn-primary" > {vote_average}</a >
             </div >
         </div >

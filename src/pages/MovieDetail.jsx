8358eb253e3704986f272fed71653d7ff6ee376e
mustafa3306 <mustafaworkkok@gmail.com>
2022-07-19 16:09:13 +0300

    cmt

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import {useLocation} from "react-router-dom"
 
-const MovieDetail = ({}) => {
-    const location = useLocation();
-    console.log(location.state.title);
+const MovieDetail = () => {
     return (
         <div>
             
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRouter = () => {
-    return (
-        <div>PrivateRouter</div>
-    )
-}
+  const user = JSON.parse(sessionStorage.getItem('user'));
 
-export default PrivateRouter
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
